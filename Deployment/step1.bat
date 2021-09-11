b70e4750e3da7281b68b94310f38c825d7963ab9
Kcrik <chris.danguien@gmail.com>
2021-09-11 19:37:22 +0100

    Updated Path to Build.bat

--- Deployment/step1.bat
+++ Deployment/step1.bat
@@ -1,4 +1,4 @@
 echo Started
 dir
-"../../UnrealEngine/Engine/Build/BatchFiles/Build.bat" DarkMatterEditor Win64 Development "Game/DarkMatter/DarkMatter.uproject" -WaitMutex
+"../../../UnrealEngine/Engine/Build/BatchFiles/Build.bat" DarkMatterEditor Win64 Development "Game/DarkMatter/DarkMatter.uproject" -WaitMutex
 echo Finished
