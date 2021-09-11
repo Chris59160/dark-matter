721f6338ba4a4b457529a9dac7855c2d07fea7ea
Kcrik <chris.danguien@gmail.com>
2021-09-11 21:46:02 +0100

    Checking if teamcity variable are accessible in the batch scripts

--- Deployment/step1.bat
+++ Deployment/step1.bat
@@ -1,4 +1,6 @@
 echo Started
 dir
+echo %teamcity.build.checkoutDir%
+echo %system.agent.home.dir%
 "../../../UnrealEngine/Engine/Build/BatchFiles/Build.bat" DarkMatterEditor Win64 Development "../../../BuildAgentDarkMatter/work/d6fb9096627369d/Game/DarkMatter/DarkMatter.uproject" -WaitMutex
 echo Finished
