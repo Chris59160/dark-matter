382542c53f1c8a2ae60ef4ee8ca0317221bd6c5a
Kcrik <chris.danguien@gmail.com>
2021-09-12 00:20:23 +0100

    set the path to uproject with the current directoy, so not hardcoded

--- Deployment/step1.bat
+++ Deployment/step1.bat
@@ -5,5 +5,8 @@ echo %teamcity.build.checkoutDir%
 echo %system.agent.home.dir%
 echo %build.checkoutDir%
 echo %agent.home.dir%
+rem cd is equals to C:\Dev\Team City\BuildAgentDarkMatter\work\d6fb9096627369d
+set "uproject=%CD%/Game/DarkMatter/DarkMatter.uproject"
+echo %uproject%
 "../../../UnrealEngine/Engine/Build/BatchFiles/Build.bat" DarkMatterEditor Win64 Development "../../../BuildAgentDarkMatter/work/d6fb9096627369d/Game/DarkMatter/DarkMatter.uproject" -WaitMutex
 echo Finished
