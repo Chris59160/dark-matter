a97e6b96240a9d71b083a66596333a967bd1ef02
Kcrik <chris.danguien@gmail.com>
2021-09-12 00:46:46 +0100

    DIsplaying the first argument, checking if it works

--- Deployment/step1.bat
+++ Deployment/step1.bat
@@ -8,5 +8,6 @@ echo %agent.home.dir%
 rem cd is equals to C:\Dev\Team City\BuildAgentDarkMatter\work\d6fb9096627369d
 set "uproject=%CD%\Game\DarkMatter\DarkMatter.uproject"
 echo %uproject%
+echo %1
 "../../../UnrealEngine/Engine/Build/BatchFiles/Build.bat" DarkMatterEditor Win64 Development "%uproject%" -WaitMutex
 echo Finished
