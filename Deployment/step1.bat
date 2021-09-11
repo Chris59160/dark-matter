946e603412554d167d33cfb4efff7d97a069e676
Kcrik <chris.danguien@gmail.com>
2021-09-12 00:07:55 +0100

    removed prefix

--- Deployment/step1.bat
+++ Deployment/step1.bat
@@ -3,5 +3,7 @@ dir
 echo %CD%
 echo %teamcity.build.checkoutDir%
 echo %system.agent.home.dir%
+echo %build.checkoutDir%
+echo %agent.home.dir%
 "../../../UnrealEngine/Engine/Build/BatchFiles/Build.bat" DarkMatterEditor Win64 Development "../../../BuildAgentDarkMatter/work/d6fb9096627369d/Game/DarkMatter/DarkMatter.uproject" -WaitMutex
 echo Finished
