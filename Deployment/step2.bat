ade3cb756127fe4c8a6fd293fbfe68d81c795069
Kcrik <chris.danguien@gmail.com>
2021-09-11 20:37:25 +0100

    updated output path

--- Deployment/step2.bat
+++ Deployment/step2.bat
@@ -1,4 +1,4 @@
 echo Started
 dir
-"../../../UnrealEngine/Engine/Build/BatchFiles/RunUAT.bat" BuildCookRun -Project="../BuildAgentDarkMatter/work/d6fb9096627369d/Game/DarkMatter/DarkMatter.uproject" -NoP4 -NoCompileEditor -Distribution -TargetPlatform=Win64 -Platform=Win64 -ClientConfig=Shipping -ServerConfig=Shipping -Cook -Build -Stage -Pak -Archive -ArchiveDirectory="../../../BuildAgentDarkMatter/OutputBuild" -Rocket -Prereqs -Package
+"../../../UnrealEngine/Engine/Build/BatchFiles/RunUAT.bat" BuildCookRun -Project="../BuildAgentDarkMatter/work/d6fb9096627369d/Game/DarkMatter/DarkMatter.uproject" -NoP4 -NoCompileEditor -Distribution -TargetPlatform=Win64 -Platform=Win64 -ClientConfig=Shipping -ServerConfig=Shipping -Cook -Build -Stage -Pak -Archive -ArchiveDirectory=".../BuildAgentDarkMatter/OutputBuild" -Rocket -Prereqs -Package
 echo Finished
