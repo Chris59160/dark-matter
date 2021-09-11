59573ca8166f80e3eceda961ebd204cde40467fb
Kcrik <chris.danguien@gmail.com>
2021-09-11 23:55:39 +0100

    Showing current path

--- Deployment/step1.bat
+++ Deployment/step1.bat
@@ -1,5 +1,6 @@
 echo Started
 dir
+echo %CD%
 echo %teamcity.build.checkoutDir%
 echo %system.agent.home.dir%
 "../../../UnrealEngine/Engine/Build/BatchFiles/Build.bat" DarkMatterEditor Win64 Development "../../../BuildAgentDarkMatter/work/d6fb9096627369d/Game/DarkMatter/DarkMatter.uproject" -WaitMutex
