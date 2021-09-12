93ce05aa6713b4cbfe74073cc9b09b38f9719c12
Kcrik <chris.danguien@gmail.com>
2021-09-12 13:22:06 +0100

    Try to use [assomg [ara,eters tp tje bat fo;e

--- Deployment/step1.bat
+++ Deployment/step1.bat
@@ -1,15 +1,12 @@
 echo Started
 dir
 echo %CD%
-echo %teamcity.build.checkoutDir%
-echo %system.agent.home.dir%
-echo %build.checkoutDir%
-echo %agent.home.dir%
 rem cd is equals to C:\Dev\Team City\BuildAgentDarkMatter\work\d6fb9096627369d
 set "uproject=%CD%\Game\DarkMatter\DarkMatter.uproject"
 echo %uproject%
 echo %1
 set "batfile=%1\..\UnrealEngine\Engine\Build\BatchFiles\Build.bat"
 echo "%batfile%"
-"../../../UnrealEngine/Engine/Build/BatchFiles/Build.bat" DarkMatterEditor Win64 Development "%uproject%" -WaitMutex
+"%batfile%" DarkMatterEditor Win64 Development "%uproject%" -WaitMutex
+rem "../../../UnrealEngine/Engine/Build/BatchFiles/Build.bat" DarkMatterEditor Win64 Development "%uproject%" -WaitMutex
 echo Finished
