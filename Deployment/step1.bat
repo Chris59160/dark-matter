c1dfd77579c146ff02aa203b6f74e4856e51ae36
Kcrik <chris.danguien@gmail.com>
2021-09-12 13:18:22 +0100

    Trying to pass the dir from team city

--- Deployment/step1.bat
+++ Deployment/step1.bat
@@ -9,5 +9,7 @@ rem cd is equals to C:\Dev\Team City\BuildAgentDarkMatter\work\d6fb9096627369d
 set "uproject=%CD%\Game\DarkMatter\DarkMatter.uproject"
 echo %uproject%
 echo %1
+set "batfile=%1\..\UnrealEngine\Engine\Build\BatchFiles\Build.bat"
+echo "%batfile%"
 "../../../UnrealEngine/Engine/Build/BatchFiles/Build.bat" DarkMatterEditor Win64 Development "%uproject%" -WaitMutex
 echo Finished
