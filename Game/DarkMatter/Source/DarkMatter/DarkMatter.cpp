24d486b4039d354788b9c323e5e52dddb84fb2d1
AndyManns <andymanns@hotmail.com>
2022-06-30 15:54:45 +0100

    Updated Mines Components and created new ones for storing overlapping actors and damaging them
    Massive update that should reach the oipnt of mines working out now. Timer Mine and Proximity Mine confirmed as working.

--- Game/DarkMatter/Source/DarkMatter/DarkMatter.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "DarkMatter.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DarkMatter, "DarkMatter" );
