ff0da423fb1df8a6cb5fb5798eb56899d948bf16
AndyManns <42152337+AndyManns@users.noreply.github.com>
2022-08-07 14:03:49 +0100

    new whitebox assets added
    added meateors, light and warp ships

--- Game/DarkMatter 4.27/Intermediate/Build/Win64/DarkMatterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ Game/DarkMatter 4.27/Intermediate/Build/Win64/DarkMatterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.29.30133
-#include "C:/Users/chris/Documents/Unreal Projects/dark-matter/Game/DarkMatter 4.27/Intermediate/Build/Win64/DarkMatterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+// Compiler: 14.29.30139
+#include "C:/Andy/dark-matter/Game/DarkMatter 4.27/Intermediate/Build/Win64/DarkMatterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
